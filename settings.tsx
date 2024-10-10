e59fbe9b9570db53f74415397b23de53e7370404
cvf223 <34798414+cvf223@users.noreply.github.com>
2024-10-10 06:18:59 +0200

    Update settings.tsx
    jo

--- settings.tsx
+++ settings.tsx
@@ -3,12 +3,12 @@
 export const mintText = [
   { label: "WL", mintText: "WL mint paying with SOL", buttonLabel: "Mint now!", header: "WL Mint" },
   { label: "OG", mintText: "Mint Paying with ABC", buttonLabel: "Mint now!", header: "OG Mint" },
-  { label: "publi", mintText: "Mint Paying with DEF", buttonLabel: "Mint now!", header: "Public Mint" },
+  { label: "public", mintText: "Mint Paying with DEF", buttonLabel: "Mint now!", header: "Public Mint" },
   { label: "default", mintText: "default mintText", buttonLabel: "Mint now!", header: "Default"}
 ];
 
 //header image in the ui. replace with your own
-export const image = "https://avatars.githubusercontent.com/u/93528482?v=4";
+export const image = "https://avatars.githubusercontent.com/u/34798414?v=5";
 
 //website title
-export const headerText = "MarkSackerberg's mint UI";
+export const headerText = "Jin the Shiba NFT minting";
